417701dcd790c764d3ca0a910c1710db44748e5e
Akshit <akshittrivedi1005@gmail.com>
2020-12-05 10:56:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+console.log('Jay shree ram');
+
+
+setInterval(() => {
+ 
+    if(window.scrollY>820)
+    {
+        let html=document.getElementById('html');
+        
+        
+    
+    }
+
+}, 100);
+
+
+
